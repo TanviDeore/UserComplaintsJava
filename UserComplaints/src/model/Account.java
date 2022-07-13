89dd6c9ca39734ed15e5c6882cc89f0eb794acc1
tanvi <tanvi@DESKTOP-I42ODHL>
2022-07-13 13:31:20 +0530

    Added Account

---
+++ UserComplaints/src/model/Account.java
@@ -0,0 +1,19 @@
+package model;
+
+public class Account {
+int accountNo;
+String name;
+public int getAccountNo() {
+	return accountNo;
+}
+public void setAccountNo(int accountNo) {
+	this.accountNo = accountNo;
+}
+public String getName() {
+	return name;
+}
+public void setName(String name) {
+	this.name = name;
+}
+
+}
